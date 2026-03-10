ed53e15d871af77122254c9d70dc6ee02798aa87
phung232010-cloud <phung232010@gmail.com>
2026-03-10 23:40:49 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,27 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { componentTagger } from "lovable-tagger";
 
-export default defineConfig(({ mode }) => ({
-  base: mode === "production" ? "/l-phi-u-4-0/" : "/",
-
-  server: {
-    host: "::",
-    port: 8080,
-    hmr: {
-      overlay: false,
-    },
-  },
-
-  plugins: [
-    react(),
-    mode === "development" && componentTagger(),
-  ].filter(Boolean),
-
+export default defineConfig({
+  base: "/L-PHI-U-4.0.COM/",
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-}));
+});
